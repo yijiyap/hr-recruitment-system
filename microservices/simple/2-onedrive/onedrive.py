10aca413cd5ccc8502fbbc2feca5244d7fdcef29
yijiyap <yijiyap@gmail.com>
2024-05-16 14:47:03 +0700

    onedrive is so HARD

--- microservices/simple/2-onedrive/onedrive.py
+++ microservices/simple/2-onedrive/onedrive.py
@@ -1,34 +1,69 @@
 import requests
 from flask import Flask, jsonify
+import os
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/list_files')
-def list_files():
-    tenant_id = 'YOUR_TENANT_ID'
-    client_id = 'YOUR_CLIENT_ID'
-    client_secret = 'YOUR_CLIENT_SECRET'
+# @app.route('/list_files')
+# def list_files():
+#     tenant_id = os.getenv('AZURE_TENANT_ID')
+#     client_id = os.getenv('AZURE_CLIENT_ID')
+#     client_secret = os.getenv('AZURE_CLIENT_SECRET_ID')
 
-    # obtain access token
-    token_url = f'https://login.microsoftonline.com/{tenant_id}/oauth2/v2.0/token'
-    token_data = {
-        'grant_type': 'client_credentials',
-        'client_id': client_id,
-        'client_secret': client_secret,
-        'scope': 'https://graph.microsoft.com/.default'
-    }
-    token_response = requests.post(token_url, data=token_data)
-    access_token = token_response.json().get('access_token')
+#     print(tenant_id, client_id, client_secret)
 
-    # list files in OneDrive
-    graph_url = 'https://graph.microsoft.com/v1.0/me/drive/root/children'
-    headers = {
-        'Authorization': f'Bearer {access_token}'
-    }
-    graph_response = requests.get(graph_url, headers=headers)
-    files = graph_response.json().get('value')
+    # # obtain access token
+    # token_url = f'https://login.microsoftonline.com/{tenant_id}/oauth2/v2.0/token'
+    # token_data = {
+    #     'grant_type': 'client_credentials',
+    #     'client_id': client_id,
+    #     'client_secret': client_secret,
+    #     'scope': 'https://graph.microsoft.com/.default'
+    # }
+    # token_response = requests.post(token_url, data=token_data)
+    # access_token = token_response.json().get('access_token')
 
-    return jsonify(files)
+    # # list files in OneDrive
+    # graph_url = 'https://graph.microsoft.com/v1.0/me/drive/root/children'
+    # headers = {
+    #     'Authorization': f'Bearer {access_token}'
+    # }
+    # graph_response = requests.get(graph_url, headers=headers)
+    # files = graph_response.json().get('value')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    # return jsonify(files)
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+tenant_id = os.getenv('AZURE_TENANT_ID')
+# tenent_id = "consumers"
+client_id = os.getenv('AZURE_CLIENT_ID')
+client_secret = os.getenv('AZURE_CLIENT_SECRET_VAlUE')
+
+print(tenant_id, client_id, client_secret)
+
+# obtain access token
+token_url = f'https://login.microsoftonline.com/{tenant_id}/oauth2/v2.0/token'
+token_data = {
+    
+    'grant_type': 'client_credentials',
+    'client_id': client_id,
+    'client_secret': client_secret,
+    'scope': 'https://graph.microsoft.com/.default'
+}
+token_response = requests.post(token_url, data=token_data)
+access_token = token_response.json().get('access_token')
+
+print(token_response.json())
+
+# # list files in OneDrive
+# graph_url = 'https://graph.microsoft.com/v1.0/me/drive/root/children'
+# headers = {
+#     'Authorization': f'Bearer {access_token}'
+# }
+# graph_response = requests.get(graph_url, headers=headers)
+# files = graph_response.json().get('value')

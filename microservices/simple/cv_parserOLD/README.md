20903387ebd7b6bf5fabcbae83ead2cb771424d1
yijiyap <yijiyap@gmail.com>
2024-05-30 11:54:44 +0700

    ping 5003 works now

---
+++ microservices/simple/cv_parserOLD/README.md
@@ -0,0 +1,3 @@
+# CV parser Microservice
+This microservice is responsible for 
+    1. Parsing the CVs into readable format for the complex microservice
--- microservices/simple/demand_survey/demand_survey.py
+++ microservices/simple/demand_survey/demand_survey.py
@@ -51,4 +51,4 @@ def upload_excel():
             # return response.json()
 
 if __name__ == "__main__":
-    app.run(port=5003, debug=True)
+    app.run(host="0.0.0.0", port=5003, debug=True)

bbb3804f13abaf230807026b8a46944180399c14
yijiyap <yijiyap@gmail.com>
2024-06-10 10:45:16 +0700

    intitialised docker

--- microservices/complex/shortlist/shortlist.py
+++ microservices/complex/shortlist/shortlist.py
@@ -9,3 +9,14 @@ from flask import (
     request,
     jsonify
 )
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/ping", methods=["GET"])
+def ping():
+    return "pong"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=9001, debug=True)
--- microservices/simple/demand_survey/demand_survey.py
+++ microservices/simple/demand_survey/demand_survey.py
@@ -62,3 +62,5 @@ def upload_excel():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5003, debug=True)
+
+    

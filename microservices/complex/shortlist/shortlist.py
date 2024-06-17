af85ff8a1b0ed66c1e6635ddfcbf9903d852df36
yijiyap <yijiyap@gmail.com>
2024-06-17 16:56:05 +0700

    updated formula for ranking candidates to the jd

--- microservices/complex/shortlist/shortlist.py
+++ microservices/complex/shortlist/shortlist.py
@@ -10,6 +10,7 @@ from flask import (
     jsonify
 )
 from flask_cors import CORS
+from . import shortlist_utils
 
 app = Flask(__name__)
 CORS(app)
@@ -26,28 +27,10 @@ def shortlist():
     # get all candidates info from the candidate microservice
     candidates_info = requests.get("http://localhost:9002/all").json()
 
-    # get the scores of all the candidates
-    score_dict = calculate_scores(ds_info, candidates_info)
+    # calculate fitness score for each candidate, and rank them
+    shortlisted_candidates = shortlist_utils.rank_candidates(ds_info, candidates_info)
 
-    # sort the scores
-    sorted_scores = sorted(score_dict.items(), key=lambda x: x[1], reverse=True)
-
-    # return the top 20 candidates
-    return sorted_scores[:20]
-
-def calculate_scores(ds_info, candidates_info):
-    """
-    Calculate the scores of all the candidates based on the demand survey info.
-    """
-    score_dict = {}
-    # for candidate in candidates_info:
-    #     score = 0
-    #     for key, value in ds_info.items():
-    #         if key in candidate:
-    #             if candidate[key] == value:
-    #                 score += 1
-    #     score_dict[candidate["candidate_id"]] = score
-    return score_dict
+    return jsonify(shortlisted_candidates)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=9001, debug=True)

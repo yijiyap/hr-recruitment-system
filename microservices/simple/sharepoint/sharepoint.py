aa86bde020ccae983c262a8f6f9ee8cc983833e2
yijiyap <yijiyap@gmail.com>
2024-06-20 17:30:08 +0700

    updated sharepoint

--- microservices/simple/sharepoint/sharepoint.py
+++ microservices/simple/sharepoint/sharepoint.py
@@ -57,6 +57,41 @@ def all():
     This endpoint will be called by the `candidates` microservice to get the job application info of all candidates.
     When this endpoint is called, the `sharepoint` microservice will call the SharePoint API and get all the job application info from SharePoint.
 
-    It will return the job application info of all candidates in the following format:
+    It will return the job application info of all candidates in the following JSON format:
     {
         "
+        "
+    }
+    """
+    # GRAPH API CODE HERE
+
+    # Convert the PDF urls to base64 encoded strings
+
+@app.route("/sharepoint/cv/all", methods=["GET"])
+def all_cv():
+    """
+    This endpoint will be called by the `candidates` microservice to get the CVs of all candidates.
+    When this endpoint is called, the `sharepoint` microservice will call the SharePoint API and get all the CVs from SharePoint.
+
+    It will return the CVs of all candidates in PDF format.
+    """
+    # GRAPH API CODE HERE
+
+    # Convert the PDF urls to base64 encoded strings
+    files_base64 = []
+    for file_url in pdf_urls:
+        response = requests.get(file_url)
+        if response.status_code == 200:
+            files_base64.append(response.content)
+
+    # Send the base64 encoded PDFs to the Next.js microservice
+    headers = {'Content-Type' : 'application/json'}
+    cv_parser_url = "http://localhost:5003/api/all"
+    try:
+        response = requests.post(cv_parser_url, json={"files": files_base64}, headers=headers)
+        if response.status_code == 200:
+            return jsonify({"message": "The cv of all candidates has been successfully consolidated.", "data": response.json()})    
+    except Exception as e:
+        return jsonify({"message": "An error occurred while consolidating the cv of all candidates."})
+
+    return jsonify({"message": "This endpoint will return the cv of all candidates in PDF format."})

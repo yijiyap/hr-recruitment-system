14e28905931d4d68891b2eee4ae72ba68270576f
yijiyap <yijiyap@gmail.com>
2024-06-20 09:51:05 +0700

    updated candidate to sharepoint calling

--- microservices/simple/demand_survey/demand_survey.py
+++ microservices/simple/demand_survey/demand_survey.py
@@ -46,7 +46,6 @@ def upload_excel():
 
             # # return response.json()
 
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5003, debug=True)
 
--- microservices/simple/sharepoint/sharepoint.py
+++ microservices/simple/sharepoint/sharepoint.py
@@ -5,39 +5,58 @@ import asyncio
 from graph import Graph
 import configparser
 from msgraph.generated.models.o_data_errors.o_data_error import ODataError
+import asyncio
+from flask_cors import CORS
 
-# create an instance of the Graph class
-async def main():
-    print('Starting the Graph API')
+# # create an instance of the Graph class
+# async def main():
+#     print('Starting the Graph API')
 
-    # Load settings from config file
-    config = configparser.ConfigParser()
-    config.read('config.cfg')
-    azure_settings = config['azure']
+#     # Load settings from config file
+#     config = configparser.ConfigParser()
+#     config.read('config.cfg')
+#     azure_settings = config['azure']
 
-    # Create an instance of the Graph class
-    graph: Graph = Graph(azure_settings)
+#     # Create an instance of the Graph class
+#     graph: Graph = Graph(azure_settings)
 
-    try:
-        # Get the access token
-        await display_access_token(graph)
+#     try:
+#         # Get the access token
+#         await display_access_token(graph)
 
-        # await display_files(graph)
-        print('Displayed files')
+#         # await display_files(graph)
+#         print('Displayed files')
     
-    except ODataError as odata_error:
-        print('Error:')
-        if odata_error.error:
-            print(odata_error.error.code, odata_error.error.message)
+#     except ODataError as odata_error:
+#         print('Error:')
+#         if odata_error.error:
+#             print(odata_error.error.code, odata_error.error.message)
 
-async def display_access_token(graph: Graph):
-    # Get the files
-    token = await graph.get_user_token()
-    print("User token:", token, "\n")
+# async def display_access_token(graph: Graph):
+#     # Get the files
+#     token = await graph.get_user_token()
+#     print("User token:", token, "\n")
 
-async def display_files(graph: Graph):
-    # Get the files
-    # TODO
-     return
+# async def display_files(graph: Graph):
+#     # Get the files
+#     # TODO
+#      return
 
-asyncio.run(main())
+# asyncio.run(main())
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/ping", methods=["GET"])
+def ping():
+    return "pong"
+
+@app.route("/sharepoint/job_app/all", methods=["GET"])
+def all():
+    """
+    This endpoint will be called by the `candidates` microservice to get the job application info of all candidates.
+    When this endpoint is called, the `sharepoint` microservice will call the SharePoint API and get all the job application info from SharePoint.
+
+    It will return the job application info of all candidates in the following format:
+    {
+        "

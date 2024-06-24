a2ed81a7af42d7b29fd70b31e1c31d6c5ceb4142
yijiyap <yijiyap@gmail.com>
2024-06-24 17:55:11 +0700

    base entity for sharepoint

--- frontend/middleware/auth.global.ts
+++ frontend/middleware/auth.global.ts
@@ -5,26 +5,26 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
   if (import.meta.server) return;
   if (to.name === "/login") return;
 
-  // const msAuth = useMSAuth();
-  // const accounts = msAuth.getAccounts();
-  // const userStore = useAppUser();
-  // const accessToken = await msAuth.acquireTokenSilent();
-  // const isAuthenticated = msAuth.isAuthenticated() && accessToken;
+  const msAuth = useMSAuth();
+  const accounts = msAuth.getAccounts();
+  const userStore = useAppUser();
+  const accessToken = await msAuth.acquireTokenSilent();
+  const isAuthenticated = msAuth.isAuthenticated() && accessToken;
 
-  // if (isAuthenticated) {
-  //   const user = {
-  //     ...accounts[0],
-  //     bearerToken: accessToken,
-  //   };
+  if (isAuthenticated) {
+    const user = {
+      ...accounts[0],
+      bearerToken: accessToken,
+    };
 
-  //   localStorage.setItem("user", JSON.stringify(user));
-  //   userStore.value.user = user;
-  // }
-  // if (to.name !== "login" && !isAuthenticated) {
-  //   return navigateTo("/login", { replace: true });
-  // } else if (to.name === "login" && isAuthenticated) {
-  //   return navigateTo("/", { replace: true });
-  // } else {
-  //   return;
-  // }
+    localStorage.setItem("user", JSON.stringify(user));
+    userStore.value.user = user;
+  }
+  if (to.name !== "login" && !isAuthenticated) {
+    return navigateTo("/login", { replace: true });
+  } else if (to.name === "login" && isAuthenticated) {
+    return navigateTo("/", { replace: true });
+  } else {
+    return;
+  }
 });
--- microservices/simple/sharepoint/sharepoint.py
+++ microservices/simple/sharepoint/sharepoint.py
@@ -2,97 +2,86 @@ import requests
 from flask import Flask, jsonify
 import os
 import asyncio
-from graph import Graph
-import configparser
-from msgraph.generated.models.o_data_errors.o_data_error import ODataError
 import asyncio
 from flask_cors import CORS
+from O365 import Account, MSGraphProtocol, FileSystemTokenBackend
+from dotenv import load_dotenv
 
-# # create an instance of the Graph class
-# async def main():
-#     print('Starting the Graph API')
+# only for testing
+load_dotenv('hr-recruitment-system/.env')
 
-#     # Load settings from config file
-#     config = configparser.ConfigParser()
-#     config.read('config.cfg')
-#     azure_settings = config['azure']
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+REDIRECT_URI = os.getenv("REDIRECT_URI")
+TENANT_ID = os.getenv("TENANT_ID")
+SCOPE = ["User.Read"]
 
-#     # Create an instance of the Graph class
-#     graph: Graph = Graph(azure_settings)
+credentials = (CLIENT_ID, CLIENT_SECRET)
 
-#     try:
-#         # Get the access token
-#         await display_access_token(graph)
+# token storage
+cur_path = os.path.dirname(os.path.realpath(__file__))
+token_backend = FileSystemTokenBackend(token_path=cur_path, token_filename='o365_token.txt')
 
-#         # await display_files(graph)
-#         print('Displayed files')
-    
-#     except ODataError as odata_error:
-#         print('Error:')
-#         if odata_error.error:
-#             print(odata_error.error.code, odata_error.error.message)
+# with your own identity
+account = Account(credentials, auth_flow_type='credentials', tenant_id=TENANT_ID, token_backend=token_backend)
+if account.authenticate():
+   print('Authenticated!')
 
-# async def display_access_token(graph: Graph):
-#     # Get the files
-#     token = await graph.get_user_token()
-#     print("User token:", token, "\n")
-
-# async def display_files(graph: Graph):
-#     # Get the files
-#     # TODO
-#      return
+SHAREPOINT_SITE_ID = os.getenv("SHAREPOINT_SITE_ID")
+sharepoint = account.sharepoint()
+root_site = sharepoint.get_root_site()
 
 # asyncio.run(main())
 
-app = Flask(__name__)
-CORS(app)
+# app = Flask(__name__)
+# CORS(app)
 
-@app.route("/ping", methods=["GET"])
-def ping():
-    return "pong"
+# @app.route("/ping", methods=["GET"])
+# def ping():
+#     return "pong"
 
-@app.route("/sharepoint/job_app/all", methods=["GET"])
-def all():
-    """
-    This endpoint will be called by the `candidates` microservice to get the job application info of all candidates.
-    When this endpoint is called, the `sharepoint` microservice will call the SharePoint API and get all the job application info from SharePoint.
+# @app.route("/sharepoint/job_app/all", methods=["GET"])
+# def all():
+#     """
+#     This endpoint will be called by the `candidates` microservice to get the job application info of all candidates.
+#     When this endpoint is called, the `sharepoint` microservice will call the SharePoint API and get all the job application info from SharePoint.
 
-    It will return the job application info of all candidates in the following JSON format:
-    {
-        "
-        "
-    }
-    """
-    # GRAPH API CODE HERE
+#     It will return the job application info of all candidates in the following JSON format:
+#     {
+#         "
+#         "
+#     }
+#     """
+#     # GRAPH API CODE HERE
 
-    # Convert the PDF urls to base64 encoded strings
-    pass
+#     # Convert the PDF urls to base64 encoded strings
+#     pass
 
-@app.route("/sharepoint/cv/all", methods=["GET"])
-def all_cv():
-    """
-    This endpoint will be called by the `candidates` microservice to get the CVs of all candidates.
-    When this endpoint is called, the `sharepoint` microservice will call the SharePoint API and get all the CVs from SharePoint.
+# @app.route("/sharepoint/cv/all", methods=["GET"])
+# def all_cv():
+#     """
+#     This endpoint will be called by the `candidates` microservice to get the CVs of all candidates.
+#     When this endpoint is called, the `sharepoint` microservice will call the SharePoint API and get all the CVs from SharePoint.
 
-    It will return the CVs of all candidates in PDF format.
-    """
-    # GRAPH API CODE HERE
+#     It will return the CVs of all candidates in PDF format.
+#     """
+#     # GRAPH API CODE HERE
 
-    # Convert the PDF urls to base64 encoded strings
-    files_base64 = []
-    for file_url in pdf_urls:
-        response = requests.get(file_url)
-        if response.status_code == 200:
-            files_base64.append(response.content)
+#     # Convert the PDF urls to base64 encoded strings
+#     files_base64 = []
+#     for file_url in pdf_urls:
+#         response = requests.get(file_url)
+#         if response.status_code == 200:
+#             files_base64.append(response.content)
 
-    # Send the base64 encoded PDFs to the Next.js microservice
-    headers = {'Content-Type' : 'application/json'}
-    cv_parser_url = "http://localhost:5003/api/all"
-    try:
-        response = requests.post(cv_parser_url, json={"files": files_base64}, headers=headers)
-        if response.status_code == 200:
-            return jsonify({"message": "The cv of all candidates has been successfully consolidated.", "data": response.json()})    
-    except Exception as e:
-        return jsonify({"message": "An error occurred while consolidating the cv of all candidates."})
+#     # Send the base64 encoded PDFs to the Next.js microservice
+#     headers = {'Content-Type' : 'application/json'}
+#     cv_parser_url = "http://localhost:5003/api/all"
+#     try:
+#         response = requests.post(cv_parser_url, json={"files": files_base64}, headers=headers)
+#         if response.status_code == 200:
+#             return jsonify({"message": "The cv of all candidates has been successfully consolidated.", "data": response.json()})    
+#     except Exception as e:
+#         return jsonify({"message": "An error occurred while consolidating the cv of all candidates."})
 
-    return jsonify({"message": "This endpoint will return the cv of all candidates in PDF format."})
+#     return jsonify({"message": "This endpoint will return the cv of all candidates in PDF format."})
